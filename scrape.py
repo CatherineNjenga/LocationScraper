d91b61b3f89e50102426d4a2d0d5897efdd54c7e
Catherine Njenga <freshiah.catherine@gmail.com>
2023-11-01 17:56:07 +0300

    Cleaning up and adding important comments

--- scrape.py
+++ scrape.py
@@ -16,11 +16,11 @@ import csv
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 
+#zip codes excel worksgeet
 file_path = r"C:\Users\Toshiba\Desktop\HelloData\zip_code_database.xlsx"
 
 # DataFrames
 df = pd.read_excel(file_path,  usecols='B,H', dtype='string')
-#df_pham = pd.DataFrame(columns=['Pharmacy Name', 'address', 'phone number', 'website', 'business hours', 'pham services'])
 header = ['Pharmacy Name', 'address', 'phone number', 'website', 'business hours', 'pham services']
 
 with open('ScraperDataSet.csv', 'w', newline='', encoding='UTF8') as f:
@@ -30,7 +30,7 @@ with open('ScraperDataSet.csv', 'w', newline='', encoding='UTF8') as f:
 data_len = len(df)
 count = 0
 
-# webpage
+# webpage scraper
 def scraper():
     driver.get("https://www.bullseyelocations.com/pages/locator-with-services-vR?f=1")
     text_input = driver.find_element("id","txtCityStateZip")
@@ -38,72 +38,61 @@ def scraper():
     text_input.send_keys(df.zip[count])
     dropdown = Select(driver.find_element("name", "ctl00$ContentPlaceHolder1$radiusList"))
     dropdown.select_by_value("50")
-    #dropdown2 = Select(driver.find_element("name", "ctl00$ContentPlaceHolder1$ddlCategories"))
-    #dropdown2.select_by_value("50")
+
+    """
+        useful if one needs to manually click on the search button
+        Currently the website auto clicks the search button
+    """
     #search_button = driver.find_element("id", "ContentPlaceHolder1_searchButton2")
     #search_button.click()
-    #driver.page_source
-    #try-except for timed-out error
+
+    #try-except block for timed-out error
     try:
         time = 10
+        # Alternative method but less effective in preventing program crashes.
         #WebDriverWait(driver, time).until(EC.visibility_of_element_located((By.CLASS_NAME, 'jcarousel-item')))
         WebDriverWait(driver, timeout=10, poll_frequency=0.5, ignored_exceptions=[TimeoutException]).until(EC.visibility_of_element_located((By.CLASS_NAME, "jcarousel-item")))
     except:
         pass
+
+    # Use BeautifulSoup to get the webpage
     page = BeautifulSoup(driver.page_source, "html.parser")
     phams_container = page.find("ul", class_="resultsList")
-
-    #phams_container = page.find(id ="resultsCarouselWide")
-    #print(phams_container)
     phams_list = phams_container.find_all("li", class_="jcarousel-item")
-    #print(phams_list)
 
     for pham in phams_list:
-        #pham = item.find("div", class_="itemWrap")
         name = pham.find("h3").text.strip()
         address = pham.find("span", itemprop="streetAddress").text.strip()
         telephone = pham.find("span", itemprop="telephone").text.strip()
         website = pham.find("a", id="website").attrs['href'].strip()
         hours_container = pham.find("div", itemprop="openingHoursSpecification")
         business_hours = hours_container.find("meta").attrs["content"]
-        #print("name", name)
-        #print("address", address)
-        #print("telephone", telephone)
-        #print("website", website)
-        #print("business hours", business_hours)
         services = pham.find("ul", class_="categoryWrapInner")
         services_list = services.find_all("li")
         pham_details = [name, address, telephone, website, business_hours]
-        #pham_service = []
         services = ''
-        #length = len(df_pham)
-        #pham_details = [name, address, telephone, website, business_hours]
+
         for service in services_list:
             pham_services = service.find_all("span", class_="categoryLabel")
+
             for pham in pham_services:
                 pham = pham.text
                 services += pham + ', '
-        print(services)      
+
+        # Truncate the trailing comma and space on the last element on services string     
         services = services[:-2]
         pham_details.append(services)  
-        #pham_details = [name, address, telephone, website, business_hours, pham_service]
-        #print(pham_details)
-                #df_pham
-        #print(length)
-        #df_pham.loc[length] = pham_details
-        #time.sleep(120)
-        #pham_details = [name, address, telephone, website, business_hours, pham_service]
-
+       
+        # Save the scraped details in a csv file created previously
         import csv
         with open('ScraperDataSet.csv', 'a', newline='', encoding='UTF8') as f:
             writer = csv.writer(f)
             writer.writerow(pham_details)
 
+        # Not necessary since chrome auto closes the driver, might be useful for other browsers.
+        #driver.close()
 
-#count = count + 1
-
-      #driver.close()
-
+# Automation of zip-codes access from the excel worksheet
 while(count < data_len):
     scraper()
     count = count + 1
